c232407f31302e23efd020f5a8ac602cdf770e24
f3Llx <45992148+f3Llx@users.noreply.github.com>
2019-01-16 12:32:40 +0000

    Se añaden los Comentarios
    Se añaden los Comentarios :cactus:

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link type="text/css" rel="stylesheet" href="css/main.css" />
     <link type="text/css" rel="stylesheet" href="css/hover.css" />
     <script type="text/javascript" src="js/main.js"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 
 
@@ -21,10 +22,10 @@
 
 ?>
 
-    <body>
+<body>
 
-        <nav class="navbar navbar bg-navBar divs_css" style="border-width:0px;">
-            <span class="hvr-sink">
+    <nav class="navbar navbar bg-navBar divs_css" style="border-width:0px;">
+        <span class="hvr-sink">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <a class="navbar-brand " href="#"><span class="navbar-txt">ICSITTER</span></a>
@@ -34,43 +35,75 @@
                     <li><a href="#"><span class="navbar-txt">Page 1</span></a></li>
                 </ul>
             </div>
-            </span>
-        </nav>
+        </span>
+    </nav>
 
-        <br><br><br>
-        <div class="container-fluid">
-            <div class="row firebg divs_css3">
-                <div class="col-md-12">
-                
-                <center><div class="neon hvr-shrink">ICSITTER </div></center>
-                
+    <br><br><br>
+    <div class="container-fluid">
+        <div class="row firebg divs_css3">
+            <div class="col-md-12">
+
+                <center>
+                    <div class="neon hvr-shrink">ICSITTER </div>
+                </center>
+
+            </div>
+        </div>
+        <br><br><br><br><br><br><br><br>
+        <div class="row">
+            <div class="col-md-8 ">
+                <div class="divs_css2 div-scroll scrollbar" id="first_div">
+                    
+                    <div  class="w3-container w3-card w3-white w3-round w3-margin"><br>
+                        <img src="img/phoenix.png" alt="Avatar" class="w3-left w3-circle w3-margin-right hvr-rotate" style="width:60px">
+                        <span class="w3-right w3-opacity">"TIME"</span>
+                        <h4>USERNAME</h4><br>
+                        <hr class="w3-clear">
+                        <p>MESSAGE Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+
                 </div>
             </div>
-            <hr><br><br><br><br><br><br><br><br>
-            <div class="row">
-                <div class="col-md-8 ">
-                    <div class="divs_css2">hola </div>
-                </div>
-                <div class="col-md-4 ">
-                    <div class="divs_css2">
-                        <div class="container">
-                            <h2 class="text-default-me">Insert your ICSTWEET:</h2>
-                            <form>
-                                <div class="form-group" >
-                                    <label for="usr" class="text-default-me">Name:</label>
-                                    <input type="text" class="form-control" style="width: 25%;">
-                                    <div class="form-group">
-                                        <label for="comment" class="text-default-me">Comment:</label>
-                                        <textarea style="width: 35%;" class="form-control" rows="5" id="comment"></textarea>
-                                    </div>
+            <div class="col-md-4 ">
+                <div class="divs_css2" >
+                    <div class="container">
+                        <h2 class="text-default-me">Insert your ICSTWEET:</h2>
+                        <form>
+                            <div class="form-group">
+                                <label for="usr" class="text-default-me">Name:</label>
+                                <input type="text" class="form-control" style="width: 25%;">
+                                <div class="form-group">
+                                    <label for="comment" class="text-default-me">Comment:</label>
+                                    <textarea style="width: 35%;" class="form-control" rows="5" id="comment"></textarea>
+                                    <br>
+                                    <button class="btn draw-border">Submit</button>
+                                    
                                 </div>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
+                        <button id="btn2">TEST BUTTON</button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-    </body>
+</body>
 
 </html>
+<script>
+  $("#btn2").click(function(){
+    console.log("hola2F");
+    console.log("...");
+    $("#first_div").append(
+                    
+                    '<div id="first_div" class="w3-container w3-card w3-white w3-round w3-margin"><br>' +
+                        '<img src="img/phoenix.png" alt="Avatar" class="w3-left w3-circle w3-margin-right hvr-rotate" style="width:60px">'+
+                        '<span class="w3-right w3-opacity">TIME</span>'+
+                        '<h4>USERNAME</h4><br>'+
+                        '<hr class="w3-clear">'+
+                        '<p>MESSAGE Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'+
+                    '</div>'
+
+                    );
+  });</script>
